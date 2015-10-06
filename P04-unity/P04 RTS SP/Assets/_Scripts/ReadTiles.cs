84628b4afad9bd24af152aab43c3c1aa3c19fb56
sandrockman <svc-student@RC335G08.student.directory.jccc>
2015-10-06 12:58:59 -0500

    Assets created, hit detection working
    Created base tile, settlement, and set up hit detection for tile, then
    raised colliders for the sides and corners. Tested on a single tile.

---
+++ P04-unity/P04 RTS SP/Assets/_Scripts/ReadTiles.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ReadTiles : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetMouseButtonDown(0))
+		{
+			//Debug.Log ("Pressed left mouse button.");
+			Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+			RaycastHit tileHit;
+
+			if(Physics.Raycast(ray,out tileHit)){
+				if(tileHit.collider.tag == "Tile"){
+					//hit.collider.gameObject now refers to the 
+					//cube under the mouse cursor if present
+					Debug.Log("Tile clicked on.");
+				}
+
+				if(tileHit.collider.tag == "Side"){
+					TileArea areaScript = tileHit.collider.GetComponent<TileArea>();
+
+					Debug.Log("Side. Clicked on " + areaScript.location);
+				}
+
+				if(tileHit.collider.tag == "Corner"){
+					TileArea areaScript = tileHit.collider.GetComponent<TileArea>();
+
+					Debug.Log ("Corner. Clicked on " + areaScript.location);
+				}
+			}
+		}
+	}
+}
